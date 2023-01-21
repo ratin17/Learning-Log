bbb278cb8c2341706deeb0c80be648283973f767
ratin17 <ratin.cse@gmail.com>
2023-01-21 14:08:58 +0600

    added add_new_topic

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -14,6 +14,10 @@ path('topics/', views.topics, name='topics'),
 #Individual Topic
 path('topics/<int:topic_id>/', views.topic, name='topic'),
 
+# Page for adding a new topic.
+path('new_topic/', views.new_topic, name='new_topic'),
+
+
 
 
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic, Entry
+from .forms import TopicForm
+
 
 # Create your views here.
 
@@ -27,3 +29,20 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
